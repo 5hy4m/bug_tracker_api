60efc4e81b48512886b8d77376185f1d090d7151
shyamkumar-code <shyam@mykinderpass.com>
2021-03-14 22:42:32 +0530

    Integrated Token Authentication

--- bug_tracker/Board/views.py
+++ bug_tracker/Board/views.py
@@ -1,7 +1,9 @@
 from bug_tracker.models import Board
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny,IsAuthenticatedOrReadOnly
 from bug_tracker.Board.serializers import BoardSerializer
-
 class BoardViewSet(viewsets.ModelViewSet):
+    permission_class = (IsAuthenticatedOrReadOnly)
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
+    
